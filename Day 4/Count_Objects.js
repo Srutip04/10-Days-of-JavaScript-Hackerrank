b63a8242d7e71ed14e9851d188f3382d310913e7
Srutip04 <subuhishruti75@gmail.com>
2021-09-06 19:46:56 +0530

    other approach

--- Day 4/Count_Objects.js
+++ Day 4/Count_Objects.js
@@ -53,3 +53,15 @@ function main() {
 
     console.log(getCount(objects));
 }
+
+
+// Other approach
+/* function getCount(objects) {
+    var ctr = 0;
+    for(var i=0; i<objects.length; i++){
+      if(objects[i].x == objects[i].y) {
+        ctr++;
+      }
+    }
+    return ctr;
+} */
